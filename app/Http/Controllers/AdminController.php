5b7cdcbb2c6fce413c50c0e4f53bc4129b66dea3
MKh_ <90140800+KuroMKh@users.noreply.github.com>
2025-07-14 01:00:23 +0800

    test baru masuk

--- app/Http/Controllers/AdminController.php
+++ app/Http/Controllers/AdminController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 use App\Models\User;
+use App\Models\StudentInformation;
 use App\Models\Meeting;
 use App\Models\Attendance;
 use Illuminate\Support\Facades\Auth;
@@ -36,9 +37,24 @@ class AdminController extends Controller
 
     public function list()
     {
-        $listalluser = User::select(['matrix_no', 'name', 'position'])->get();
+        $listalluser = User::select([
+            'users.matrix_no',
+            'users.name',
+            'users.email',
+            'studentinformation.profile_picture',
+            'studentinformation.phone_number',
+            'studentinformation.date_of_birth',
+            'studentinformation.address',
+            'studentinformation.year',
+            'studentinformation.year',
+            'studentinformation.bio',
+            'studentinformation.gender',
+            'studentinformation.position'
+        ])
+            ->join('studentinformation', 'users.matrix_no', '=', 'studentinformation.matrix_no')
+            ->get();
         $usercount = $listalluser->count();
-        $positiondropdown = User::select('position')->distinct()->pluck('position');
+        $positiondropdown = StudentInformation::select('position')->distinct()->pluck('position');
 
         // Pass the data to the view
         return view('manage_member.list-all-mem-dashboard', compact('listalluser', 'usercount', 'positiondropdown'));
